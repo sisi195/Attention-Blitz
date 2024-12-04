daa6b69d54c66dc6e28f0527489bd0289213fa4c
Sierra Gordon <sierra.gordon1684@stu.mtec.edu>
2024-12-03 17:54:58 -0700

    Create git_commands.py

---
+++ git_commands.py
@@ -0,0 +1,17 @@
+!DOCTYPE py>
+<!-- saved from url=(0088)https://github.com/sisi195/Attention-Blitz/blob/main/new_directory/mkdir%20new_directory -->
+<html lang="en" data-color-mode="auto" data-light-theme="light" data-dark-theme="dark" data-a11y-animated-images="system" data-a11y-link-underlines="true" data-qb-installed="true" data-js-focus-visible="" data-turbo-loaded=""><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style type="text/css">.turbo-progress-bar {
+  position: fixed;
+  display: block;
+  top: 0;
+  left: 0;
+  height: 3px;
+  background: #0076ff;
+  z-index: 2147483647;
+  transition:
+    width 300ms ease-out,
+    opacity 150ms 150ms ease-in;
+  transform: translate3d(0, 0, 0);
+}
+</style>
+    
